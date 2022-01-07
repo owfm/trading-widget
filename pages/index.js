26a943792149779b9a0476a47fc132f7bbb48c82
Oliver Mansell <Oliver.Mansell@StoneX.com>
2022-01-07 11:42:32 +0000

    fix: buy/sell alerts

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ const App = () => {
         </select>
       </header>
       <section className="price-container">
-        <button className="price-box buy">
+        <button onClick={() => window.alert("buy")} className="price-box buy">
           <h2>BUY</h2>
           <section className="price">
             <div className="most-sig">
@@ -93,7 +93,7 @@ const App = () => {
           </AnimatePresence>
         </button>
 
-        <button className="price-box sell">
+        <button onClick={() => window.alert("sell")} className="price-box sell">
           <h2>SELL</h2>
           <section className="price">
             <div className="most-sig">
