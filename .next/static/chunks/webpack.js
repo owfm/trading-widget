06453b7828369956e6987bbd62b83e903000d3bf
Oliver Mansell <Oliver.Mansell@StoneX.com>
2022-01-07 11:58:06 +0000

    expand icon tweak

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fff63abe19890839"; }
+/******/ 		__webpack_require__.h = function() { return "8c276a0d3373ac26"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
